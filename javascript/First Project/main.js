fc3cc2b23ad2d58cb89d9ca786143b12b3f3da5a
Muhammad Mustafa Hussain <140386879+muhammadmustafarooqi@users.noreply.github.com>
2024-09-15 13:16:34 +0500

    Add files via upload

---
+++ javascript/First Project/main.js
@@ -0,0 +1,38 @@
+alert("Welcome to javascript")
+
+//  Numbers Multiplication with let
+let a = 15;
+let b = 100;
+let c = a * b;
+console.log (c);
+
+// Name Addition with var
+var p = "Muhammad";
+var q = " Mustafa";
+var t = p + q;
+console.log (t); 
+
+// Name+ Age with const
+const o = "Muhammad Mustafa";
+const u = " Age";
+const h = (o + u +17);
+console .log (h);
+
+const price1 = 13;
+const price2 = 15;
+let total = (price1 + price2);
+console.log (total)
+
+// Illegal
+
+// Starts with a digit
+// let 2price = 20;
+
+// Contains a space
+// var = My varibale = a;
+
+// Contains a hyphen
+// var my-variable = b
+
+// Reserved word
+// let abc = if;
